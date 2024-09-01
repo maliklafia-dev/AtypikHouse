961a10a67bae846ee248d7fedd51ab9d7a211b2e
malik LAFIA <maliksero.lafia@gmail.com>
2024-09-01 18:34:13 +0200

    Ajouter boutton de connexion ok - Message d'accueil, boutton de déconnection ok - Boutton siteMap (footer) ok - Bandeau de cookies (A voir) ok - Découvrir les activités de l'entreprise ok/mittigé - Modifier le texte d'acceptation des conditions pour les formulaires. ok - Système de filtre ok - Boutton de recherche sur la page de liste des appartements ok - possiblité de commenter une annonce ok - Ajouter un message de confirmation d'ajout à la newsletter (juste au dessus du formulaire) ok

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ hamburgerButton.addEventListener("click", () => {
 
 /* hide menu when users click on link */
 const links = document.querySelectorAll("link-one15");
-console.log(links);
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
@@ -17,3 +16,40 @@ links.forEach((link) => {
     column61.classList.remove("active");
   });
 });
+
+/*Profil popup*/
+
+const profilIcon = document.getElementById("profilIcon");
+
+profilIcon.addEventListener("click", () => {
+  const profilContainer = document.getElementById("profilContainer");
+  profilContainer.classList.toggle("active");
+});
+
+/*Gestion des coockies*/
+document.addEventListener("DOMContentLoaded", function () {
+  const consentGiven = localStorage.getItem("cookieConsent");
+  if (!consentGiven) {
+    document.getElementById("cookie-consent-popup").style.display = "block";
+  }
+});
+
+document
+  .getElementById("accept-cookies")
+  .addEventListener("click", function () {
+    localStorage.setItem("cookieConsent", "accepted");
+    document.getElementById("cookie-consent-popup").classList.toggle("active");
+    // Placez ici le code pour activer les cookies ou services qui nécessitent le consentement
+  });
+
+document
+  .getElementById("decline-cookies")
+  .addEventListener("click", function () {
+    localStorage.setItem("cookieConsent", "declined");
+    document.getElementById("cookie-consent-popup").classList.toggle("active");
+    // Placez ici le code pour désactiver les cookies ou services qui nécessitent le consentement
+  });
+
+if (localStorage.getItem("cookieConsent") === "accepted") {
+  // Initialiser Google Analytics ici
+}
